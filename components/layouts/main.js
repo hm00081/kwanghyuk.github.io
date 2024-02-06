745121a4b8d5ae3078470603dfe4277c975f6fff
문광혁 <moonkwanghyuk@mungwanghyeog-ui-MacBookAir.local>
2024-02-07 00:39:03 +0900

    0207

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -20,14 +20,14 @@ const Main = ({ children, router }) => {
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:title" content="Kwanghyuk Moon" />
-        <meta name="twitter:card" content="summary_large_image" />
+        {/* <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
+        <meta name="twitter:creator" content="@craftzdog" /> */}
+        {/* <meta name="twitter:image" content="https://www.craftz.dog/card.png" /> */}
         <meta property="og:site_name" content="Kwanghyuk Moon" />
         <meta name="og:title" content="Kwanghyuk Moon" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        {/* <meta property="og:image" content="https://www.craftz.dog/card.png" /> */}
         <title>Kwanghyuk Moon - Homepage</title>
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
